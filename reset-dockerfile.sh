e35a3b72bc655a80c8183d68657bbff1678e28bd
Yuta Sakurai <sakurai.yuta@gmail.com>
2021-03-12 18:48:16 +0000

    added env which package-manager use(conda or mamba), remove timestamp from patch file, fix conda channel

--- reset-dockerfile.sh
+++ reset-dockerfile.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-declare -a IMAGES=(base-notebook datascience-notebook minimal-notebook r-notebook scipy-notebook tensorflow-notebook all-spark-notebook pyspark-notebook)
+declare -a IMAGES=(base-notebook datascience-notebook minimal-notebook r-notebook scipy-notebook all-spark-notebook pyspark-notebook)
 
 for IMAGE in ${IMAGES[@]}; do
   cp master/${IMAGE}/Dockerfile ./${IMAGE}
